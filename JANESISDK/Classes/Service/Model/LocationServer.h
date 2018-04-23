cda852f05c417ae3e968ebd1fbfee8c2d80f4138
shuangquan <butrys@163.com>
2018-04-23 10:10:00 +0800

    first commit

---
+++ JANESISDK/Classes/Service/Model/LocationServer.h
@@ -0,0 +1,12 @@
+//
+//  LocationServer.h
+//  AFNetworking
+//
+//  Created by apple on 2018/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationServer : NSObject
+
+@end
---
+++ JANESISDK/Classes/Service/Model/LocationServer.m
@@ -0,0 +1,12 @@
+//
+//  LocationServer.m
+//  AFNetworking
+//
+//  Created by apple on 2018/4/20.
+//
+
+#import "LocationServer.h"
+
+@implementation LocationServer
+
+@end
